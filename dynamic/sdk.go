9b461061683b6964537d28dab022e233d689fb99
Ayan Jhunjhunwala <ayanqwerty564@gmail.com>
2025-11-24 15:21:19 -0800

    Testing and fetching from SDK

--- dynamic/sdk.go
+++ dynamic/sdk.go
@@ -3,27 +3,34 @@ package main
 import (
 	"context"
 	"log"
-	"github.com/docker/docker/api/types"
-    "github.com/docker/docker/client"
 
+	"github.com/docker/docker/api/types/container" // NEW: Needed for the options (container.ListOptions)
+	"github.com/docker/docker/client"
 )
 
 func test() {
-
 	ctx := context.Background()
+
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		log.Fatalf("Error creating Docker client: %v", err)
+	}
+	defer cli.Close()
 
+	containers, err := cli.ContainerList(ctx, container.ListOptions{All: true})
+	if err != nil {
+		log.Fatalf("Error listing containers: %v", err)
 	}
 
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
-    if err != nil {
-        log.Fatalf("Error listing containers: %v", err)
-    }
+	for _, ctr := range containers {
+		id := ctr.ID
+		if len(id) > 12 {
+			id = id[:12]
+		}
+		log.Printf("%s  %s  %v  %s", id, ctr.Image, ctr.Names, ctr.Status)
+	}
+}
 
-
-	for _, container := range containers {
-		log.Printf("%s  %s  %v  %s", container.ID[:12], container.Image, container.Names, container.Status)
-    }
+func main() {
+	test()
 }
