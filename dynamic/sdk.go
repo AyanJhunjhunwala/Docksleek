a9b19ba36a0a8b7522c54cd40832993155554a45
Ayan Jhunjhunwala <ayanqwerty564@gmail.com>
2025-11-28 12:28:05 -0800

    added layer analysis

--- dynamic/sdk.go
+++ dynamic/sdk.go
@@ -1,36 +1,60 @@
 package main
-
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
-	"github.com/docker/docker/api/types/container" // NEW: Needed for the options (container.ListOptions)
 	"github.com/docker/docker/client"
 )
 
-func test() {
+func AnalyzeImageLayers(imageName string) error {
 	ctx := context.Background()
 
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	cli, err := client.NewClientWithOpts(
+		client.FromEnv,
+		client.WithAPIVersionNegotiation(),
+	)
 	if err != nil {
-		log.Fatalf("Error creating Docker client: %v", err)
+		return err
 	}
 	defer cli.Close()
 
-	containers, err := cli.ContainerList(ctx, container.ListOptions{All: true})
+	history, err := cli.ImageHistory(ctx, imageName)
 	if err != nil {
-		log.Fatalf("Error listing containers: %v", err)
+		return err
 	}
 
-	for _, ctr := range containers {
-		id := ctr.ID
-		if len(id) > 12 {
-			id = id[:12]
+	fmt.Printf("Layers for image %s:\n\n", imageName)
+
+	// history is from newest layer to oldest
+	var cumulativeSize int64
+	for i, h := range history {
+		layerSize := h.Size
+		cumulativeSize += layerSize
+
+		created := time.Unix(h.Created, 0)
+
+		fmt.Printf("Layer #%d\n", len(history)-i-1)
+		fmt.Printf("  ID:        %s\n", h.ID)
+		fmt.Printf("  Created:   %s\n", created.Format(time.RFC3339))
+		fmt.Printf("  Size:      %.2f MB\n", float64(layerSize)/(1024*1024))
+		fmt.Printf("  Cumulative: %.2f MB\n", float64(cumulativeSize)/(1024*1024))
+		fmt.Printf("  CreatedBy: %s\n", h.CreatedBy)
+		if h.Comment != "" {
+			fmt.Printf("  Comment:   %s\n", h.Comment)
 		}
-		log.Printf("%s  %s  %v  %s", id, ctr.Image, ctr.Names, ctr.Status)
+		fmt.Println()
 	}
+
+	return nil
 }
 
+
 func main() {
-	test()
+
+	// Can update this with all images available in the folder
+	if err := AnalyzeImageLayers("docksleek:latest"); err != nil {
+		log.Fatal(err)
+	}
 }
